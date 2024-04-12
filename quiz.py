616372460bfabd4c0619f3f51bbb6696874a1090
alexander voeler <avoeler@gmail.com>
2024-04-12 19:14:27 +0900

    Made a simple quiz game.

---
+++ quiz.py
@@ -0,0 +1,28 @@
+print("Welcome to the quiz!")
+
+playing = input("Do you want to play? ")
+print(playing)
+
+if playing.lower != "yes":
+    quit()
+
+print("Okay! Let's play :)")
+score = 0
+
+answer = input("What does CPU stand for? ")
+
+if answer.lower == "central processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does GPU stand for? ")
+
+if answer.lower == "graphics processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+print("Thanks for playing!")
